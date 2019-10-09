fd41efe76adf60ccb8d2ee15fa055e47763d03fd
Andrzej <azbroja@outlook.com>
2019-10-09 16:26:06 +0200

    add working hours table

---
+++ database/migrations/2019_03_24_102432_create_work_hours_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateWorkHoursTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('work_hours', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->unsignedInteger('user_id');
+            $table->foreign('user_id')->references('id')->on('users');
+            $table->integer('telephone_hours')->nullable();
+            $table->integer('working_hours')->default(8);
+            $table->integer('name_of_hours')->nullable();
+            $table->date('date');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('work_hours');
+    }
+}
